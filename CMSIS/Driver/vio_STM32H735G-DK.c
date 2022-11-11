cd575b79b887f96f253e3e38d830ad42e7b31270
Ziga Mahne <ziga.mahne@arm.com>
2022-11-11 13:26:29 +0100

    Update VIO CMSIS-Driver
    - correct variables initialization to avoid compiler warnings

--- CMSIS/Driver/vio_STM32H735G-DK.c
+++ CMSIS/Driver/vio_STM32H735G-DK.c
@@ -1,8 +1,8 @@
 /******************************************************************************
  * @file     vio_STM32H735G-DK.c
  * @brief    Virtual I/O implementation for board STM32H735G-DK
- * @version  V1.1.0
- * @date     30. March 2022
+ * @version  V1.1.1
+ * @date     8. November 2022
  ******************************************************************************/
 /*
  * Copyright (c) 2021-2022 Arm Limited (or its affiliates).
@@ -380,7 +380,7 @@ void vioSetIPv4 (uint32_t id, vioAddrIPv4_t addrIPv4) {
 // Get IPv4 address input.
 vioAddrIPv4_t vioGetIPv4 (uint32_t id) {
   uint32_t index = id;
-  vioAddrIPv4_t addrIPv4 = {0U, 0U, 0U, 0U};
+  vioAddrIPv4_t addrIPv4 = { {0U, 0U, 0U, 0U} };
 #if !defined CMSIS_VIN
 // Add user variables here:
 
@@ -425,8 +425,8 @@ void vioSetIPv6 (uint32_t id, vioAddrIPv6_t addrIPv6) {
 // Get IPv6 address input.
 vioAddrIPv6_t vioGetIPv6 (uint32_t id) {
   uint32_t index = id;
-  vioAddrIPv6_t addrIPv6 = {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
-                            0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U};
+  vioAddrIPv6_t addrIPv6 = { {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
+                              0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U} };
 #if !defined CMSIS_VIN
 // Add user variables here:
 
